d1d88cbe7f8a1c845b9189fce2297c5d1db9b214
Hans Bering <9629717+hansi-b@users.noreply.github.com>
2022-03-29 19:51:10 +0200

    removed FadeTransition

--- simplerTimes/src/main/java/org/hansib/simplerTimes/ButtonsStripController.java
+++ simplerTimes/src/main/java/org/hansib/simplerTimes/ButtonsStripController.java
@@ -2,12 +2,9 @@ package org.hansib.simplerTimes;
 
 import org.kordamp.ikonli.javafx.FontIcon;
 
-import javafx.animation.Animation;
-import javafx.animation.FadeTransition;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
-import javafx.util.Duration;
 
 public class ButtonsStripController {
 
@@ -34,28 +31,14 @@ public class ButtonsStripController {
 		stopButton.setGraphic(Icons.stop());
 		stopButton.setDisable(true);
 
-		FadeTransition startFt = getFade(startButton);
-
 		startButton.setOnAction(a -> {
 			startButton.setDisable(true);
-			startFt.play();
 			stopButton.setDisable(false);
 		});
 
 		stopButton.setOnAction(a -> {
 			stopButton.setDisable(true);
 			startButton.setDisable(false);
-			startFt.stop();
 		});
-	}
-
-
-	private static FadeTransition getFade(Button btn) {
-		FadeTransition ft = new FadeTransition(Duration.millis(1200), btn);
-		ft.setFromValue(.6);
-		ft.setToValue(0.15);
-		ft.setCycleCount(Animation.INDEFINITE);
-		ft.setAutoReverse(true);
-		return ft;
 	}
 }
