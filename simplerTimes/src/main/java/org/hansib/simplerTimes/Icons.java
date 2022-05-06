fc1ac9dba769cc412c0579ece3411173fd259c03
Hans Bering <9629717+hansi-b@users.noreply.github.com>
2022-05-07 00:24:58 +0200

    refactoring: extracted stuff
    * methods, Icons

---
+++ simplerTimes/src/main/java/org/hansib/simplerTimes/Icons.java
@@ -0,0 +1,16 @@
+package org.hansib.simplerTimes;
+
+import org.kordamp.ikonli.javafx.FontIcon;
+
+import javafx.scene.Node;
+
+class Icons {
+
+	static final Node start() {
+		return new FontIcon("codicon-play-circle");
+	}
+
+	static final Node stop() {
+		return new FontIcon("codicon-stop-circle");
+	}
+}
