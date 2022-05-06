eaa09c38bfecc35ed84ba860675e51bd27446ed3
Hans Bering <9629717+hansi-b@users.noreply.github.com>
2022-05-07 00:24:58 +0200

    refactoring: extracted stuff
    * methods, Icons

--- simplerTimes/src/main/java/org/hansib/simplerTimes/ButtonsStripController.java
+++ simplerTimes/src/main/java/org/hansib/simplerTimes/ButtonsStripController.java
@@ -10,11 +10,9 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.hansib.simplerTimes.times.Span;
 import org.hansib.simplerTimes.times.Timer;
-import org.kordamp.ikonli.javafx.FontIcon;
 
 import javafx.application.Platform;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
@@ -23,17 +21,6 @@ public class ButtonsStripController {
 	private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
 
 	private static final Logger log = LogManager.getLogger();
-
-	private static class Icons {
-
-		private static final Node start() {
-			return new FontIcon("codicon-play-circle");
-		}
-
-		private static final Node stop() {
-			return new FontIcon("codicon-stop-circle");
-		}
-	}
 
 	@FXML
 	Button startButton;
@@ -59,28 +46,30 @@ public class ButtonsStripController {
 		stopButton.setGraphic(Icons.stop());
 		stopButton.setDisable(true);
 
-		startButton.setOnAction(a -> {
-			startButton.setDisable(true);
-			stopButton.setDisable(false);
-
-			timer.start();
-			scheduleAtFixedRate = scheduler.scheduleAtFixedRate(
-					() -> Platform.runLater(() -> elapsedTime.setText(fmt(timer.currentDuration()))), 0, 40,
-					TimeUnit.MILLISECONDS);
-		});
-
-		stopButton.setOnAction(a -> {
-			stopButton.setDisable(true);
-			startButton.setDisable(false);
-
-			scheduleAtFixedRate.cancel(true);
-
-			Span span = timer.stopAndGet();
-			log.info(span);
-		});
+		startButton.setOnAction(a -> startTiming());
+		stopButton.setOnAction(a -> stopTiming());
 	}
 
-	private static String fmt(Duration d) {
+	private void startTiming() {
+		startButton.setDisable(true);
+		stopButton.setDisable(false);
+		timer.start();
+		scheduleAtFixedRate = scheduler.scheduleAtFixedRate(
+				() -> Platform.runLater(() -> elapsedTime.setText(fmtTime(timer.currentDuration()))), 0, 40,
+				TimeUnit.MILLISECONDS);
+	}
+
+	private void stopTiming() {
+		stopButton.setDisable(true);
+		startButton.setDisable(false);
+
+		scheduleAtFixedRate.cancel(true);
+
+		Span span = timer.stopAndGet();
+		log.info(span);
+	}
+
+	private static String fmtTime(Duration d) {
 		return String.format("%d:%02d:%02d", d.toHours(), d.toMinutesPart(), d.toSecondsPart());
 	}
 }
