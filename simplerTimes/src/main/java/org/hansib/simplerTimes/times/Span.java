f1486b8b543957feb187651a1259f4909b701065
Hans Bering <9629717+hansi-b@users.noreply.github.com>
2022-05-07 15:22:28 +0200

    require non-null constructor arguments

--- simplerTimes/src/main/java/org/hansib/simplerTimes/times/Span.java
+++ simplerTimes/src/main/java/org/hansib/simplerTimes/times/Span.java
@@ -1,12 +1,16 @@
 package org.hansib.simplerTimes.times;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 import org.hansib.sundries.Errors;
 
 public record Span(LocalDateTime start, LocalDateTime end) {
 
 	public Span {
+		Objects.requireNonNull(start);
+		Objects.requireNonNull(end);
+
 		if (start.compareTo(end) > 0)
 			throw Errors.illegalArg("Start (%s) cannot be after end (%s)");
 	}
