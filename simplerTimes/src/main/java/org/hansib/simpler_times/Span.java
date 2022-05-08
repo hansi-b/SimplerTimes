727f1f343f048106a129cb0d3835d8c872a942d0
Hans Bering <9629717+hansi-b@users.noreply.github.com>
2022-05-08 13:49:32 +0200

    changed Span to use OffsetDateTime

--- simplerTimes/src/main/java/org/hansib/simpler_times/Span.java
+++ simplerTimes/src/main/java/org/hansib/simpler_times/Span.java
@@ -1,15 +1,22 @@
 package org.hansib.simpler_times;
 
 import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
 
 import org.hansib.simpler_times.times.Interval;
 
-record Span(String label, LocalDateTime start, LocalDateTime end) {
-	static Span of(String label, Interval span) {
-		return new Span(label, cut(span.start()), cut(span.end()));
+record Span(String label, OffsetDateTime start, OffsetDateTime end) {
+
+	Span(String label, LocalDateTime start, LocalDateTime end) {
+		this(label, cut(start), cut(end));
 	}
 
-	private static LocalDateTime cut(LocalDateTime ldt) {
-		return ldt.withNano(0);
+	static Span of(String label, Interval span) {
+		return new Span(label, span.start(), span.end());
+	}
+
+	private static OffsetDateTime cut(LocalDateTime ldt) {
+		return ldt.withNano(0).atZone(ZoneId.systemDefault()).toOffsetDateTime();
 	}
 }
