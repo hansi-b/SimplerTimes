062cf4b7e3b91bdb763a4533e2fa108558576f2a
Hans Bering <9629717+hansi-b@users.noreply.github.com>
2022-05-08 17:53:38 +0200

    removed unused method

--- simplerTimes/src/main/java/org/hansib/simpler_times/spans/Span.java
+++ simplerTimes/src/main/java/org/hansib/simpler_times/spans/Span.java
@@ -4,16 +4,10 @@ import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 import java.time.ZoneId;
 
-import org.hansib.simpler_times.times.Interval;
-
 record Span(String label, OffsetDateTime start, OffsetDateTime end) {
 
 	Span(String label, LocalDateTime start, LocalDateTime end) {
 		this(label, cut(start), cut(end));
-	}
-
-	static Span of(String label, Interval span) {
-		return new Span(label, span.start(), span.end());
 	}
 
 	private static OffsetDateTime cut(LocalDateTime ldt) {
