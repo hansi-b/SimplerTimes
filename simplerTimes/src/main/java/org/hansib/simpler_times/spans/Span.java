98e58d2712df0e5069e51c83af79d4a3fcb05ca2
Hans Bering <9629717+hansi-b@users.noreply.github.com>
2022-05-08 18:23:17 +0200

    added proper nanoseconds rounding in Span
    * instead of just cutting

--- simplerTimes/src/main/java/org/hansib/simpler_times/spans/Span.java
+++ simplerTimes/src/main/java/org/hansib/simpler_times/spans/Span.java
@@ -3,14 +3,29 @@ package org.hansib.simpler_times.spans;
 import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
+import java.util.Objects;
 
-record Span(String label, OffsetDateTime start, OffsetDateTime end) {
+import org.hansib.sundries.Errors;
 
-	Span(String label, LocalDateTime start, LocalDateTime end) {
-		this(label, cut(start), cut(end));
+public record Span(String label, OffsetDateTime start, OffsetDateTime end) {
+
+	private static final int nanosToAdd = 1000000000 / 2;
+
+	public Span {
+		Objects.requireNonNull(start);
+		Objects.requireNonNull(end);
+
+		if (start.compareTo(end) >= 0)
+			throw Errors.illegalArg("End (%s) must be after start (%s)", end, start);
 	}
 
-	private static OffsetDateTime cut(LocalDateTime ldt) {
-		return ldt.withNano(0).atZone(ZoneId.systemDefault()).toOffsetDateTime();
+	public Span(String label, LocalDateTime start, LocalDateTime end) {
+		this(label, round(start), round(end));
+	}
+
+	private static OffsetDateTime round(LocalDateTime ldt) {
+		return ldt.plusNanos(nanosToAdd).truncatedTo(ChronoUnit.SECONDS).atZone(ZoneId.systemDefault())
+				.toOffsetDateTime();
 	}
 }
