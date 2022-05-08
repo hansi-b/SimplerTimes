ad80e5f1a2682baaf0a2f7be22134ab41efe9af1
Hans Bering <9629717+hansi-b@users.noreply.github.com>
2022-05-08 18:31:12 +0200

    trigger start button on ENTER in project field
    * also: add Span to collection on stop

--- simplerTimes/src/main/java/org/hansib/simpler_times/TimesMainController.java
+++ simplerTimes/src/main/java/org/hansib/simpler_times/TimesMainController.java
@@ -2,11 +2,14 @@ package org.hansib.simpler_times;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.hansib.simpler_times.spans.Span;
 import org.hansib.simpler_times.spans.SpansCollection;
 import org.hansib.simpler_times.times.Interval;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 
 public class TimesMainController {
 
@@ -20,7 +23,7 @@ public class TimesMainController {
 
 	private final SpansCollection spans;
 
-	TimesMainController() {
+	public TimesMainController() {
 		this(new SpansCollection());
 	}
 
@@ -31,9 +34,17 @@ public class TimesMainController {
 	@FXML
 	void initialize() {
 		buttonsStripController.setIntervalReceiver(this::handleInterval);
+
+		projectField.addEventHandler(KeyEvent.KEY_PRESSED, e -> {
+			if (e.getCode() == KeyCode.ENTER) {
+				buttonsStripController.startButton.requestFocus();
+				buttonsStripController.startButton.fire();
+			}
+		});
 	}
 
 	private void handleInterval(Interval t) {
 		log.info("{} {}", projectField.getText(), t);
+		spans.add(new Span(projectField.getText(), t.start(), t.end()));
 	}
 }
