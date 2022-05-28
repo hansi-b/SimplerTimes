694d31b403f96bcf687919854ffa123e5e58bf56
Hans Bering <9629717+hansi-b@users.noreply.github.com>
2022-05-28 15:53:30 +0200

    renamed Node -> TreeNode
    * now uses Nameable
    * added PrettyPrinter, adapted and added tests

---
+++ simplerTimesCore/src/main/java/org/hansib/simplertimes/Project.java
@@ -0,0 +1,10 @@
+package org.hansib.simplertimes;
+
+import org.hansib.simplertimes.tree.Nameable;
+
+public class Project extends Nameable {
+
+	public Project(String name) {
+		super(name);
+	}
+}
