5da5936bf9a5ecb2482b76264ecc7b835869b2da
Hans Bering <9629717+hansi-b@users.noreply.github.com>
2022-06-07 19:51:26 +0200

    (de)serialising of project trees

--- simplerTimesCore/src/main/java/org/hansib/simplertimes/tree/TreeNode.java
+++ simplerTimesCore/src/main/java/org/hansib/simplertimes/tree/TreeNode.java
@@ -20,6 +20,12 @@ public class TreeNode<E extends Nameable> {
 		this.children = children;
 	}
 
+	public static <F extends Nameable> TreeNode<F> connected(F element, List<TreeNode<F>> children) {
+		TreeNode<F> node = new TreeNode<>(null, element, children);
+		node.children.forEach(c -> c.parent = node);
+		return node;
+	}
+
 	public static <F extends Nameable> TreeNode<F> root() {
 		return new TreeNode<>(null, null, new ArrayList<>());
 	}
