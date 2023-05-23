cf6f84d78f3ce9b6620ab0a68b74709b38f5f305
Hans Bering <9629717+hansi-b@users.noreply.github.com>
2023-05-23 15:25:09 +0200

    added date navigation to stats
    * week/month backwards and forwards

--- simplerTimesFx/src/main/java/org/hansib/simplertimes/fx/Icons.java
+++ simplerTimesFx/src/main/java/org/hansib/simplertimes/fx/Icons.java
@@ -39,4 +39,24 @@ class Icons {
 	static final Node showSpans() {
 		return new FontIcon("codicon-calendar");
 	}
+
+	static final Node weekBack() {
+		return new FontIcon("unil-angle-left");
+	}
+
+	static final Node weekForward() {
+		return new FontIcon("unil-angle-right");
+	}
+
+	static final Node monthBack() {
+		return new FontIcon("unil-angle-double-left");
+	}
+
+	static final Node monthForward() {
+		return new FontIcon("unil-angle-double-right");
+	}
+
+	static final Node today() {
+		return new FontIcon("unil-bullseye");
+	}
 }
--- simplerTimesFx/src/main/java/org/hansib/simplertimes/fx/SpansStatsController.java
+++ simplerTimesFx/src/main/java/org/hansib/simplertimes/fx/SpansStatsController.java
@@ -20,6 +20,7 @@ package org.hansib.simplertimes.fx;
 
 import java.time.Duration;
 import java.time.LocalDate;
+import java.time.Period;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
@@ -30,12 +31,15 @@ import org.apache.logging.log4j.Logger;
 import org.hansib.simplertimes.projects.Project;
 import org.hansib.simplertimes.spans.SpansCollection;
 import org.hansib.simplertimes.times.Utils;
+import org.hansib.sundries.fx.ButtonDecorator;
 
+import javafx.beans.binding.Bindings;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
@@ -59,31 +63,78 @@ public class SpansStatsController {
 	@FXML
 	private TableView<Stats> spansStats;
 
+	@FXML
+	Button monthBack;
+	@FXML
+	Button weekBack;
+	@FXML
+	Button today;
+	@FXML
+	Button weekForward;
+	@FXML
+	Button monthForward;
+
 	private SpansCollection spansCollection;
+
+	private ObjectProperty<LocalDate> dateProp;
 
 	@FXML
 	void initialize() {
 		log.info("Initialising spans stats");
 
+		TableColumn<Stats, String> projectColumn = new TableColumn<>("Project");
+		projectColumn.setCellValueFactory(data -> data.getValue().project);
+		spansStats.getColumns().add(projectColumn);
+
+		dateProp = new SimpleObjectProperty<>(LocalDate.now());
+
+		new ButtonDecorator(monthBack).graphic(Icons.monthBack()).onAction(e -> shiftDate(Period.ofMonths(-1)));
+		new ButtonDecorator(weekBack).graphic(Icons.weekBack()).onAction(e -> shiftDate(Period.ofDays(-7)));
+
+		new ButtonDecorator(today).graphic(Icons.today()).onAction(e -> setDate(LocalDate.now()));
+		today.disableProperty()
+				.bind(Bindings.createBooleanBinding(() -> LocalDate.now().equals(dateProp.get()), dateProp));
+
+		new ButtonDecorator(weekForward).graphic(Icons.weekForward()).onAction(e -> shiftDate(Period.ofDays(7)));
+		new ButtonDecorator(monthForward).graphic(Icons.monthForward()).onAction(e -> shiftDate(Period.ofMonths(1)));
 	}
 
 	public void setSpans(SpansCollection spansCollection) {
 		this.spansCollection = spansCollection;
 
-		StatsCalculator calc = new StatsCalculator(spansCollection);
+		updateStats();
+	}
 
-		SortedSet<LocalDate> allDates = Utils.daysOfCurrentWeek();
+	private void setDate(LocalDate newDate) {
+		dateProp.set(newDate);
+		updateStats();
+	}
 
-		TableColumn<Stats, String> projectColumn = new TableColumn<>("Project");
-		projectColumn.setCellValueFactory(data -> data.getValue().project);
-		spansStats.getColumns().add(projectColumn);
+	private void shiftDate(Period shift) {
+		dateProp.set(dateProp.get().plus(shift));
+		updateStats();
+	}
+
+	private void updateStats() {
+		SortedSet<LocalDate> allDates = Utils.daysOfWeek(dateProp.get());
+		updateDateColumns(allDates);
+
+		fillStats(allDates);
+	}
+
+	private void updateDateColumns(SortedSet<LocalDate> allDates) {
+		if (spansStats.getColumns().size() > 1)
+			spansStats.getColumns().remove(1, spansStats.getColumns().size());
 
 		for (LocalDate dt : allDates) {
 			TableColumn<Stats, String> odtColumn = new TableColumn<>(dt.toString());
 			odtColumn.setCellValueFactory(data -> data.getValue().ldStr(dt));
 			spansStats.getColumns().add(odtColumn);
 		}
+	}
 
+	private void fillStats(SortedSet<LocalDate> allDates) {
+		StatsCalculator calc = new StatsCalculator(spansCollection);
 		Set<Project> allProjects = calc.allProjects();
 		ObservableList<Stats> items = FXCollections.observableArrayList();
 		items.addAll(allProjects.stream().map(p -> Stats.of(p, calc.get(p, allDates))).toList());
